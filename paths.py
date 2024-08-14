5ac22e8ba5408a313dd160229638362fd42c1984
Julien Albert <julien.albert@unamur.be>
2024-08-14 18:05:52 +0200

    quick fix on paths.py to allow usage of models in subdirectories

--- paths.py
+++ paths.py
@@ -2,6 +2,13 @@ import os
 
 ROOT_PATH = os.path.abspath(".")
 PREPROCESSED_PATH = os.path.join(ROOT_PATH, "results/ml1m/preprocessed")
+
+# Quick fix to allow usage of models in subdirectories (e.g., in notebooks dir)
+# TODO need to refactor
+if not os.path.exists(PREPROCESSED_PATH):
+    ROOT_PATH = os.path.abspath("..")
+    PREPROCESSED_PATH = os.path.join(ROOT_PATH, "results/ml1m/preprocessed")
+
 PATHS = {
     "pred_paths": os.path.join(PREPROCESSED_PATH, "pgpr/pred_paths.pkl"),
     "products": os.path.join(PREPROCESSED_PATH, "products.txt"),
