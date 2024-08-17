f341fce05f1416ce6fe997f7ac12f4ec70b6dcbe
Julien Albert <julien.albert@unamur.be>
2024-08-17 11:13:18 +0200

    add llm explainer and example notebook

--- recommendation/recommenders/abstract_recommender.py
+++ recommendation/recommenders/abstract_recommender.py
@@ -28,27 +28,3 @@ class AbstractRecommender(ABC):
         :return: List[RecoPath] list of recommendation paths.
         """
         pass
-
-    #TODO need to migrate this method in explainer
-    def generate_facts(self, path: RecoPath) -> str:
-        """
-        Generate facts for a recommendation path.
-
-        :param path: RecoPath.
-        :return: str facts for a recommendation path.
-        """
-        facts_txt = "% Path: \n"
-        for rel in path.rels:
-            facts_txt += rel.to_facts() + "\n"
-            user = self.user_registry.find_by_eid(rel.in_node.entity_id)
-            product = self.product_registry.find_by_eid(rel.out_node.entity_id)
-            facts_txt += self.rating_registry.find_user_product_rating(user.uid, product.pid).to_facts() + "\n"
-        facts_txt += "% Background Knowledge: \n"
-        for node in path.nodes:
-            if node.type == "user":
-                user = self.user_registry.find_by_eid(node.entity_id)
-                facts_txt += user.to_facts() + "\n"
-            elif node.type == "product":
-                product = self.product_registry.find_by_eid(node.entity_id)
-                facts_txt += product.to_facts() + "\n"
-        return facts_txt
